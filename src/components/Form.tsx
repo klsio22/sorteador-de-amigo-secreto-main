b0949a71f0a0c289dc226d20e8074dc043620463
klesio <klesio.dev@gmail.com>
2023-02-21 11:10:58 -0300

    started tests tdd in form

---
+++ src/components/Form.tsx
@@ -0,0 +1,8 @@
+export function Form() {
+  return (
+    <div>
+      <input type='text' placeholder='Insira os nomes dos participantes' />
+      <button disabled={true}>Adicionar</button>
+    </div>
+  );
+}
--- src/test/Forms.test.tsx
+++ src/test/Forms.test.tsx
@@ -1,12 +1,12 @@
 import { render, screen } from '@testing-library/react';
-import React from 'react';
+import { Form } from '../components/Form';
 
 //Jest
 test("when input is empty,new participation can't be added", () => {
   render(<Form />);
 
   // encontrar no DOM o input
-  const input = screen.getByPlaceholderText('Isira os nome dos participantes');
+  const input = screen.getByPlaceholderText('Insira os nomes dos participantes');
   //encontrar o botão
   const button = screen.getByRole('button');
   //garantir que o input esteja no documento
